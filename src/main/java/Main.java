f6cbf8b3e240a14c027c97ecf24c81ddd50dbd9b
Stamati Morellas <stamatios.morellas@gmail.com>
2020-03-27 02:58:33 -0500

    Additions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,16 +26,23 @@ public class Main extends ListenerAdapter {
      */
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        System.out.println("We received a message from " +
-                event.getAuthor().getName() + ": " +
-                event.getMessage().getContentDisplay()
+        System.out.println("HEY, We received a message from " +
+                event.getAuthor().getName() + ": '" +
+                event.getMessage().getContentDisplay() + "'"
         );
         // If the message i s">ping", reply with "pong!"
-        if (event.getMessage().getContentRaw().equals(">ping")) {
-            String pong = "pong :)";
-            event.getChannel().sendMessage(pong).queue(); // Remember to add message to queue
-        } // If the message i s">histam", reply with message
-        if (event.getMessage().getContentRaw().equals(">histam")) {
+        if (event.getMessage().getContentRaw().equals("hibot")) {
+            String r1 = "Hello, human! Did I end up on the wrong planet again?";
+            String r2 = "Hey, Cybervoyd 2.0 here, just checking in!";
+            String r3 = "Greetings :)";
+            String r4 = "Wanna know a secret? Stamati built me and deployed me to this server. He's honestly kind of a genius";
+            int s = (int) (1 + (Math.random() * (4 - 1)));
+            String toSend = "Hello, human!";
+
+            event.getChannel().sendMessage(toSend).queue(); // Remember to add message to queue
+        }
+        // If the message i s">histam", reply with message
+        if (event.getMessage().getContentRaw().equals("histam")) {
             String histam = "Hello Stamati - A.K.A Supreme Leader, DJ SHMECKLEBOTTOM, Professor Timmi, The Great Stromboli.net";
             event.getChannel().sendMessage(histam).queue(); // Remember to add message to queue
         }
